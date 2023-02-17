df03c011a8afc28da4c1c36a7a1602dbd124d78e
Arjun Hamdalah <arjun.hamdalah@moladin.com>
2023-02-17 17:26:38 +0700

    init commit

---
+++ src/test/java/com/example/midtransdemo/MainPage.java
@@ -0,0 +1,19 @@
+package com.example.midtransdemo;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+// page_url = https://demo.midtrans.com/
+public class MainPage {
+    public SelenideElement buyNowButton = $x("//*[@data-reactid='.0.0.0.2.0.0.5']");
+    public SelenideElement inputName = $x("//*[@data-reactid='.0.0.1.0.3.0.0.0.1.0']");
+    public SelenideElement inputEmail = $x("//*[@data-reactid='.0.0.1.0.3.0.0.1.1.0']");
+    public SelenideElement inputPhone = $x("//*[@data-reactid='.0.0.1.0.3.0.0.2.1.0']");
+    public SelenideElement inputCity = $x("//*[@data-reactid='.0.0.1.0.3.0.0.3.1.0']");
+    public SelenideElement inputAddress = $x("//*[@data-reactid='.0.0.1.0.3.0.0.4.1.0']");
+    public SelenideElement inputPostalCode = $x("//*[@data-reactid='.0.0.1.0.3.0.0.5.1.0']");
+    public SelenideElement checkoutButton = $(".cart-checkout");
+
+}
